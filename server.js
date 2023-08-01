a9ae4d70eb3d929e86aa9834c2179f0454f3b00f
Shuvo Sarker <sarker3664@gmail.com>
2023-08-02 02:50:38 +0600

    feat - category backend CRUD successfully done

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
 import cors from 'cors';
 
 // configure
@@ -23,6 +24,7 @@ connectDB();
 
 //* routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
 
 // check api
 app.get('/', (req, res) => {
